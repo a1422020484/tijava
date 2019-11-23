1d2aa89049b1360d3adfb7ef87ee944e5edfac98
a1422020484 <1422020484@qq.com>
2019-11-23 12:12:49 +0800

    新增基数排序

---
+++ tijava/src/tjjava/sortUtil/seven/mergeSort/RadixSort2.java
@@ -0,0 +1,42 @@
+package tjjava.sortUtil.seven.mergeSort;
+
+import java.util.Arrays;
+
+public class RadixSort2 {
+	public static void main(String[] args) {
+		int arr[] = { 10, 5, 2, 1, 4, 3, 8, 9, 23, 44, 55, 22, 11, 25, 37 };
+		rsort(arr, 2);
+		System.out.println(Arrays.toString(arr));
+	}
+
+	/**
+	 * @param arr
+	 *            待排序数组
+	 * @param n
+	 *            最大数值的位数
+	 */
+	public static void rsort(int arr[], int n) {
+		int f = 1;
+		while (n > 0) {
+			int temp[][] = new int[10][0];// 创建一个桶数组
+
+			for (int i = 0; i < arr.length; i++) {
+				int r = arr[i] / f % 10; // 每个数值各位上的余数
+				temp[r] = Arrays.copyOf(temp[r], temp[r].length + 1);
+				temp[r][temp[r].length - 1] = arr[i];
+			}
+
+			int k = 0;
+			for (int i = 0; i < temp.length; i++) {
+				for (int j = 0; j < temp[i].length; j++) {
+					if (temp[i][j] != 0) {
+						arr[k] = temp[i][j];
+						k++;
+					}
+				}
+			}
+			f *= 10;
+			n--;
+		}
+	}
+}
