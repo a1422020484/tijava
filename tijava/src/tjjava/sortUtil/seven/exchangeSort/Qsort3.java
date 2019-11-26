84c47184c14e49b7b258e8bd02b9afcb5966d6e7
a1422020484 <1422020484@qq.com>
2019-11-26 17:17:48 +0800

    原来的排序方法写错了，修改一下

--- tijava/src/tjjava/sortUtil/seven/exchangeSort/Qsort3.java
+++ tijava/src/tjjava/sortUtil/seven/exchangeSort/Qsort3.java
@@ -1,40 +1,46 @@
 package tjjava.sortUtil.seven.exchangeSort;
 
+import java.util.Arrays;
+
+/**
+ * @author yangxp av39093184 核心是在选基准值， 第一趟排序之后，将基准值左边都是小于基准值，右边都大于基准。
+ */
 public class Qsort3 {
 	public static int[] qsort(int arr[], int start, int end) {
-		int pivot = arr[start - 1];
+		int pivot = arr[end];
 		int i = start;
-		int j = end;
+		int j = end - 1;
 		while (i < j) {
-			while ((i < j) && (arr[j] > pivot)) {
-				j--;
-			}
 			while ((i < j) && (arr[i] < pivot)) {
 				i++;
 			}
-			if ((i == j)) {
-				int temp = arr[i];
-				arr[i] = pivot;
-				arr[start - 1] = temp;
+			while ((i < j) && (arr[j] > pivot)) {
+				j--;
+			}
+			if (i == j) {
+				if(arr[i] > pivot) {
+					int temp = arr[i];
+					arr[i] = pivot;
+					arr[end] = temp;
+				}
 			} else {
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
 			}
+			System.out.println(Arrays.toString(arr));
 		}
-		if (i - 1 > start)
-			arr = qsort(arr, start, i - 1);
-		if (j + 1 < end)
-			arr = qsort(arr, j + 2, end);
-		return (arr);
+		if (j - 1 > start)
+			arr = qsort(arr, 0, j);
+		if (i + 1 < end)
+			arr = qsort(arr, j, end);
+		return arr;
 	}
 
 	public static void main(String[] args) {
-		int arr[] = new int[] { 3, 2, 1, 79, 9, 8, 6, 4, 5,10,55,47,89 };
+		int arr[] = new int[] { 3, 2, 1, 5, 8, 6, 4, 9 };
 		int len = arr.length - 1;
-		arr = qsort(arr, 1, len);
-		for (int i : arr) {
-			System.out.print(i + " ");
-		}
+		arr = qsort(arr, 0, len);
+		System.out.println(Arrays.toString(arr));
 	}
 }
